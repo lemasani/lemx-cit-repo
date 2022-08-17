b6441ab4c3cadc409e9fe0fb5e89812dcca46c83
lemasani <brianlemasan@gmail.com>
2022-08-17 19:08:37 +0300

    1st lesson of python in CIT

---
+++ lesson1.py
@@ -0,0 +1,33 @@
+print("hello world")
+#this is a comment in python
+raining = True
+if raining:
+    print("it is raining now")
+
+#variables
+my_name = "lemasani"
+
+#data types
+#strings
+all_chars = "A, B, C,...Z"
+my_name = "lemasani"
+
+#numbers/integers
+my_age = 23
+
+#floating numbers
+my_weight = 80.67
+
+#boolen True/False
+is_raining = True
+
+#none is a data type that represents nothing
+dinner = None
+
+
+my_name = "lemasani"
+my_age = 23
+my_weight = 80.67
+#output it a single sentence
+
+print("My name is " + my_name + ", I am " + str(my_age) + " Years old" )
