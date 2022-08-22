2f56f2c4ba75e16146bbfab7a9d56b382ed7091a
lemasani <brianlemasan@gmail.com>
2022-08-22 19:47:16 +0300

    solved the remaining qns

--- assignment.py
+++ assignment.py
@@ -1,3 +1,6 @@
+from re import X
+
+
 name = "lemasani"
 age =  23
 weight = 80.5
@@ -29,3 +32,32 @@ while(k<=11):
     product = table * k
     print(product)
     k += 1
+# Write a program to display only those numbers from a list that satisfy the following conditions
+ # - The number must be divisible by five
+  #- If the number is greater than 150, then skip it and move to the next number
+  #- If the number is greater than 500, then stop the loop
+# given `numbers = [12, 75, 150, 180, 145, 525, 50]`
+print("new line ")
+given = [12, 75, 150, 180, 145, 525, 50]
+for number in given:
+    if ((number > 500) and (number % 5 ==0)):
+        break
+    if ((number > 150) and (number % 5 ==0)):
+        
+        continue
+    if ((number % 5 ==0) and (number >= 12)):
+        print(number)
+        
+# 5.  Write a program to count the total number of digits in a number using a while loop. given number `4673453`
+# `4673453`
+print("qn 5")
+m = 4673453
+while m > 0:
+    print(len(str(m)))
+    break
+
+# 6. Display numbers from -10 to -1 using while loop
+b = -10
+while b <= -1:
+    print(b)
+    b += 1
